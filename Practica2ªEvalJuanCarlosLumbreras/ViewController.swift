5a979aa5109873bb5dc6a09460c70657d71dc240
Juan Carlos Lumbreras Diaz <juancarlos@macbook-pro-de-juan.local>
2019-03-06 16:06:52 +0100

    Initial Commit

---
+++ Practica2ªEvalJuanCarlosLumbreras/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica2ªEvalJuanCarlosLumbreras
+//
+//  Created by Juan Carlos Lumbreras Diaz on 06/03/2019.
+//  Copyright © 2019 Juan Carlos Lumbreras Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
